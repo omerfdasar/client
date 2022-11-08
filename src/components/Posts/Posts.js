cf7e64952358c940e5eb34be29afbf818387d936
Omer Dasar <omerfarukdasar@gmail.com>
2022-11-08 20:18:51 +0100

    deploy

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
+import { useSelector } from "react-redux";
+
 import Post from "./Post/Post";
-import useStyles from "./styles.js";
-const Posts = () => {
+import useStyles from "./styles";
+
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+
+  console.log(posts);
   const classes = useStyles();
-  return (
-    <>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { applyMiddleware, compose } from "redux";
-import { configureStore } from '@reduxjs/toolkit'
+import { applyMiddleware, compose, createStore } from "redux";
+
 import thunk from "redux-thunk";
 import reducers from "./reducers/index.js";
 import App from "./App";
-// import "./index.css";
+import "./index.css";
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
     <App />
